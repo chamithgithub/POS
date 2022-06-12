8eeacc713962675c30ea87e5d78c9007f422c6e3
chamithgithub <dchamith4@gmail.com>
2022-06-12 12:14:28 +0530

    rafactord Customer exist method

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -40,4 +40,10 @@ public class CustomerDAOImpl {
         return pstm.executeUpdate()> 0;
 
     }
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
+        pstm.setString(1, id);
+        return pstm.executeQuery().next();
+    }
 }
